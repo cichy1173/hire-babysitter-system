ddcf6dc7105c498a49609701bdd9bf8022389e00
MorganMLG <cholody.kacper1998@gmail.com>
2021-10-11 23:07:07 +0200

    Added account_type and user tables

---
+++ database/migrations/2021_10_11_222600_create_users_table.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_account_type')
+                    ->constrained('account_types')
+                    ->onUpdate('cascade')
+                    ->onDelete('restrict');
+            $table->string('name', 40);
+            $table->string('surname', 50);
+            $table->string('nickname', 15)
+                    ->unique()
+                    ->nullable();
+            $table->string('email')
+                    ->unique();
+            $table->timestamp('email_verified_at')
+                    ->nullable();
+            $table->string('password', 128);
+            $table->string('salt', 128);
+            $table->string('photo', 50)
+                    ->nullable();
+            $table->float('reputation', 2, 1);
+            $table->string('about', 3000)
+                    ->nullable();
+            $table->string('schedule', 50)
+                    ->nullable();
+            $table->binary('is_blocked');
+            $table->timestamp('blocked_to')
+                    ->nullable();
+            $table->binary('is_deleted');
+            $table->binary('require_pass_reset');
+            $table->rememberToken();
+            $table->timestamps();
+            $table->index(['id', 'nickname', 'email']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
