5f52e4491d60814304b1fc872795ca6ea1cf4680
Kacper Chołody <cholody.kacper1998@gmail.com>
2021-10-27 19:54:57 +0200

    Some styling

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Advertisement;
 use App\Models\City;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Pagination\LengthAwarePaginator as Paginator;
 
@@ -11,20 +12,22 @@ class HomePageController extends Controller
 {
     public function index(Request $request)
     {
-        $results = Advertisement::select('id', 'id_advertisement_type', 'id_user', 'title', 'hour_rate', 'child_num', 'created_at')->get();
+        $results = Advertisement::select('id', 'id_advertisement_type', 'id_user', 'title', 'hour_rate', 'child_num', 'created_at')->get()->sortByDesc('created_at');
 
         $array = array();
     
         foreach ($results as $key => $value) {
             $district = Advertisement::find($value['id'])->districts[0];
             $city = City::find($district['id_city']);
+            $user = User::find($value['id_user']);
             $array[$key] = $value;
             $array[$key]['city'] = $city['city_name'];
             $array[$key]['district'] = $district['district_name'];
+            $array[$key]['user_nick'] = $user->nickname;
         }
 
         $total = count($results);
-        $per_page = 1;
+        $per_page = 5;
         $current_page = $request->input('page') ?? 1;
         $starting_point = ($current_page * $per_page) - $per_page;
 
