3a46aa67b6406560d3d43ed65b3a93ecf423a736
cichy1173 <cichy1173@protonmail.com>
2021-11-02 19:13:31 +0100

    Some changes in resetting passwords

--- resources/lang/pl/passwords.php
+++ resources/lang/pl/passwords.php
@@ -12,8 +12,8 @@
 */
 
 return [
-    'reset'     => 'Hasło zostało zresetowane!',
-    'sent'      => 'Przypomnienie hasła zostało wysłane!',
+    'reset'     => 'Hasło zostało zmienione!',
+    'sent'      => 'Wiadomość została wysłana!',
     'throttled' => 'Proszę zaczekać zanim spróbujesz ponownie.',
     'token'     => 'Token resetowania hasła jest nieprawidłowy.',
     'user'      => 'Nie znaleziono użytkownika z takim adresem e-mail.',
--- resources/views/User/index.blade.php
+++ resources/views/User/index.blade.php
@@ -23,11 +23,34 @@
                          <div class=" p-2"> 
                             <form id="delete_account_form" class="mb-1" action=" {{route('reset_pswd')}} " method="post">
                                 @csrf
-                                <button class="p-2 btn btn-outline-warning text-black-50 btn-lg btn-block font-weight-bold">{{ __('Zmień hasło') }}</button>
-
+                                <button type="button" data-toggle="modal" data-target="#changePasswordPopUp"  class="p-2 btn btn-outline-warning text-black-50 btn-lg btn-block font-weight-bold">{{ __('Zmień hasło') }}</button>
+                               
                             </form>
-   
+                            <div class="modal fade" id="changePasswordPopUp" tabindex="-1" role="dialog" aria-hidden="true" aria-labelledby="changePasswordPopUp">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="changePasswordPopUp">{{__('Czy jesteś pewien, że chcesz zmienić hasło?')}}</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <p>{{__('W celu zmiany hasła zostaniesz wylogowany.')}}</p>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">{{__('Zamknij')}}</button>
+                                            <form class="form-inline" action=" {{route('reset_pswd')}} "  method="POST">
+                                                @csrf
+            
+                                                <button class="btn btn btn-info" type="submit" id="button_change">{{__('Zmień hasło')}}</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>    
 
+            
                          
                             <form id="delete_account_form" action=" {{route('userEdit')}} " method="post">
                                 @csrf
