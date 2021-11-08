bc85896fd904e465ac1b7c044ddb68d9007bb566
MorganMLG <cholody.kacper1998@gmail.com>
2021-11-08 13:16:05 +0100

    ShowUser don't show all data from user table now

--- app/Http/Controllers/User/ShowUserController.php
+++ app/Http/Controllers/User/ShowUserController.php
@@ -10,8 +10,10 @@ class ShowUserController extends Controller
 {
     public function index(User $user)
     {
+        $user = User::select('name', 'surname', 'nickname', 'reputation', 'photo', 'about')
+                        ->where('id', $user->id)->get();
         return view('User.showUser', [
-            'user' => $user
+            'user' => $user[0]
         ]);
     }
 }
