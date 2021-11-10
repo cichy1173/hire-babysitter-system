3f6dc996bc60d195a3621c10b6139f95ec2bfac2
cichy1173 <cichy1173@protonmail.com>
2021-11-10 19:00:32 +0100

    Save work

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -7,6 +7,7 @@ use App\Models\Message;
 use Illuminate\Support\Arr;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Crypt;
 use SebastianBergmann\Environment\Console;
 use Illuminate\Contracts\Encryption\DecryptException;
--- app/Http/Controllers/User/ShowUserController.php
+++ app/Http/Controllers/User/ShowUserController.php
@@ -2,18 +2,20 @@
 
 namespace App\Http\Controllers\User;
 
-use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class ShowUserController extends Controller
 {
     public function index(User $user)
     {
-        $user = User::select('id' ,'name', 'surname', 'nickname', 'reputation', 'photo', 'about')
-                        ->where('id', $user->id)->get();
-        return view('User.showUser', [
-            'user' => $user[0]
-        ]);
+   
+            $user = User::select('id' ,'name', 'surname', 'nickname', 'reputation', 'photo', 'about')
+                            ->where('id', $user->id)->get();
+            return view('User.showUser', [
+                'user' => $user[0]
+            ]);
     }
 }
