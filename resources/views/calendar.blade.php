940de7d32b420d424f21912c0e77b6c84c813318
MorganMLG <cholody.kacper1998@gmail.com>
2021-11-18 15:01:06 +0100

    Calendar page styling

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -5,64 +5,61 @@
 <div>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-auto">
+            <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header" align="center" vlign="middle">{{ __('Grafik') }}</div>
+                    <div class="card-header text-center">{{ __('Grafik') }}</div>
     
                     <div class="card-body">
                         @auth
-                        @foreach ($cals as $cal)
-                            <div class="row">
-                                <div class="col">
-                                    <div class="card mb-3">
-                                        <div class="card-header">
-                                            <div class="d-inline mb-0 text-muted">{{__('Zleceniodawca ')}}</div>
-                                            <a href="{{route('showUser', $cal->id_user)}}" class="font-weight-bold mb-0">{{$cal->nickname}}</a>                                            
-                                        </div>
-                                        <div class="card-body ">
-                                            <div class="row h-auto" style="margin-left:1px">                                                                                                   
-                                                <div class="col h-auto">
-                                                    <div class="row d-inline-block" style="font-size: large">
-                                                        <div class=" d-inline mb-0 text-muted" style="font-weight:bold">{{__('Rozpoczęcie: ')}}</div>                                                        
-                                                        <div class="d-inline mb-0" >{{ \Carbon\Carbon::parse($cal->time_from)->translatedFormat('d F Y H:i') }}</div>                                                       
+                            @if(isset($cals) && count($cals) > 0)
+                                @foreach ($cals as $cal)
+                                    <div class="row">
+                                        <div class="col">
+                                            <div class="card mb-3">
+                                                <div class="card-header">
+                                                    <div class="d-inline mb-0 text-muted">{{__('Zleceniodawca ')}}</div>
+                                                    <a href="{{route('showUser', $cal->id_user)}}" class="font-weight-bold mb-0">{{$cal->nickname}}</a>                                            
+                                                </div>
+                                                <div class="card-body">
+                                                    <div class="row">
+                                                        <div class="col">
+                                                            <p class=" d-inline mb-0 text-muted">{{__('Rozpoczęcie: ')}}</p>                                                        
+                                                            <p class="d-inline mb-0" >{{ \Carbon\Carbon::parse($cal->time_from)->translatedFormat('d F Y H:i') }}</p>                     
+                                                        </div>     
+                                                        <div class="col">
+                                                            <p class="d-inline mb-0 text-muted">{{__(' Zakończenie: ')}}</p>
+                                                            <p class="d-inline mb-0" >{{\Carbon\Carbon::parse($cal->time_to)->translatedFormat('d F Y H:i')}}</p>
+                                                        </div>                             
                                                     </div>
-                                                    
-                                                    <div class="row ">
-                                                        <div class="d-inline mb-0 text-muted">{{__('Stawka: ')}}</div>
-                                                        <div class="d-inline mb-0 ">{{$cal->hour_rate}}</div>
-                                                        <div class="d-inline mb-0 text-muted">{{__('zl/h ')}}</div>
+                                                    <div class="row">
+                                                        <div class="col">
+                                                            <p class="d-inline mb-0 text-muted">{{__('Stawka: ')}}</p>
+                                                            <p class="d-inline mb-0 ">{{$cal->hour_rate}}</p>
+                                                            <p class="d-inline mb-0 text-muted">{{__('zl/h ')}}</p>                   
+                                                        </div>     
+                                                        <div class="col">
+                                                            <p class=" d-inline mb-0 text-muted">{{__('Lokalizacja: ')}}</p>
+                                                            <p class=" d-inline mb-0">{{$cal->district_name}}</p>
+                                                        </div>                             
                                                     </div>
-
-                                                    <div class="row ">
-                                                        <div class="d-inline mb-0 text-muted">{{__('Liczba dzieci: ')}}</div>
-                                                        <div class="d-inline mb-0">{{$cal->child_num}}</div>
+                                                    <div class="row">
+                                                        <div class="col">
+                                                            <p class="d-inline mb-0 text-muted">{{__('Liczba dzieci: ')}}</p>
+                                                            <p class="d-inline mb-0">{{$cal->child_num}}</p>                   
+                                                        </div>     
+                                                        <div class="col">
+                                                            <p class="d-inline mb-0 text-muted">{{__('Zaakceptowane: ')}}</p>
+                                                            <p class="d-inline mb-0">{{__('Tak')}}</p> 
+                                                        </div>                             
                                                     </div>
                                                 </div>
-                                                                                                                                                             
-                                                                                              
-                                                <div class="col h-auto">                                            
-                                                    <div class="row d-inline-block" style="font-size: large">
-                                                        <div class="d-inline mb-0 text-muted" style="font-weight:bold">{{__(' Zakończenie: ')}}</div>
-                                                        <div class="d-inline mb-0" >{{\Carbon\Carbon::parse($cal->time_to)->translatedFormat('d F Y H:i')}}</div>
-                                                    </div>
-                                                    <div class="row d-inline-block">
-                                                        <div class=" d-inline mb-0 text-muted">{{__('Lokalizacja: ')}}</div>
-                                                        <div class=" d-inline mb-0">{{$cal->district_name}}</div>
-                                                    </div>
-                                                    <div class="row ">
-                                                        <div class="d-inline mb-0 text-muted">{{__('Zaakceptowane: ')}}</div>
-                                                        <div class="d-inline mb-0">Tak</div> 
-                                                    </div>
-                                                </div>                                                 
-                                            </div>     
+                                            </div>
                                         </div>
-                                    </div>
-                                </div>
-                            </div>                     
-                        @endforeach
-                        
-                        
-
+                                    </div>                     
+                                @endforeach
+                            @else
+                                <p class="text-center alert alert-info mb-0">{{__('Obecnie nie masz żadnych zaakceptowanych zgłoszeń. Grafik niedostepny')}}</p>
+                            @endif
                         @endauth
 
                         @guest
